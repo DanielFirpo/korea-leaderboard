9cf696d23752a027520b58ba849cb3135b559548
Turtled <trtld2@gmail.com>
2022-05-09 20:09:03 -0700

    bugfix on links

--- korea-streamer-ranking/src/RankingListItem.js
+++ korea-streamer-ranking/src/RankingListItem.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 function RankingListItem(props) {
 
+    let streamLink = 'https://www.twitch.tv/' + encodeURIComponent(props.streamerName)
+    let opggLink = 'https://www.op.gg/summoners/kr/' + encodeURIComponent(props.accountName)
     return (
         <div className="RankingListItem">
             <div className="RankingListItemText">{props.listIndex}</div>
-            <div className="RankingListItemText"><a href={props.streamerName}></a></div>
-            <div className="RankingListItemText"><a href={props.accountName}></a></div>
+            <div className="RankingListItemText"><a href={streamLink}>{props.streamerName}</a></div>
+            <div className="RankingListItemText"><a href={opggLink}>{props.accountName}</a></div>
             <div className="RankingListItemText">{props.rankName}</div>
             <div className="RankingListItemText">{props.ladderRank}</div>
             <div className="RankingListItemText">{props.totalWins}</div>
